0fa80121bee13a9df260fed9009d7aaccad6617d
EL-KENZ <kenzlejaze@gmail.com>
2024-09-07 18:01:05 +0100

    Remove old isac/src directory

--- isac/src/assets/handouts/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = "/resources/handouts/DCE102.pdf"
-print(f"Checking existence of: {file_path}")
-
-if os.path.exists(file_path):
-    print("File exists")
-else:
-    print("File does not exist")
